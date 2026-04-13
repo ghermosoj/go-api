17b786bfdd601d580d9dce889a4dbf4936fb640d
Guillermo <guillermo.hermoso02@gmail.com>
2026-04-13 13:52:38 +0200

    feat: Some changes add to link the api with the api gateway

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -3,7 +3,24 @@ package routes
 import (
     "github.com/gin-gonic/gin"
     "github.com/ghermosoj/go-api/internal/handlers"
+    "net/http"
 )
+
+func GatewayGuard() gin.HandlerFunc {
+	return func(c *gin.Context) {
+        
+		const sharedSecret = "comunicacion-interna-segura-2026"
+		
+		if c.GetHeader("X-Internal-Secret") != sharedSecret {
+			c.JSON(http.StatusForbidden, gin.H{
+				"error": "Acceso denegado. Las peticiones deben pasar por el API Gateway.",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
 
 func SetupRouter() *gin.Engine {
     r := gin.Default()
